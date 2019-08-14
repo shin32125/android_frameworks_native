367a3f6a29ebb6b141a6541361ebfeb5a0539979
Shuzhen Wang <shuzhenwang@google.com>
2019-08-14 10:41:12 -0700

    libgui: Add discardFreeBuffers callback to producer
    This enables buffer producer to clear its buffer caches.
    
    Test: Camera CTS, libgui_test
    Bug: 136677409
    Bug: 145617243
    Change-Id: I7f46e977f3edc3b08177654531745d8ca0b86889
    Merged-In: I7f46e977f3edc3b08177654531745d8ca0b86889
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libs/gui/BufferQueueProducer.cpp
+++ libs/gui/BufferQueueProducer.cpp
@@ -1212,9 +1212,8 @@ status_t BufferQueueProducer::connect(const sp<IProducerListener>& listener,
                     }
                     mCore->mLinkedToDeath = listener;
                 }
-                if (listener->needsReleaseNotify()) {
-                    mCore->mConnectedProducerListener = listener;
-                }
+                mCore->mConnectedProducerListener = listener;
+                mCore->mBufferReleasedCbEnabled = listener->needsReleaseNotify();
             }
             break;
         default:
